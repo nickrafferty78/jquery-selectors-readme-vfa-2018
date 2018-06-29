84cf59c5c4df3f89900ae7d172fb9211b81c6876
Nick Rafferty <nickrafferty78@gmail.com>
2018-06-29 16:27:05 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,17 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p').html();
+}
+function lastImageSelector (){
+  return $('img:last');
+}
+function ninjaBabySelector(){
+  return ('#baby-ninja');
+}
+function divSelector(){
+  return $('.pics');
+}
+function firstListItem(){
+  return $('ul #pic-list:first-child');
+}
+
